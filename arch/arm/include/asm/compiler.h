27bf33b9a73954b19c486e5a437b14a9bf72bcd8
parasetam0l <skaya0@hotmail.com>
2026-08-06 01:17:11 +0300

    ARM: compiler.h: disable __asmeq register check for gcc 9

--- arch/arm/include/asm/compiler.h
+++ arch/arm/include/asm/compiler.h
@@ -9,7 +9,9 @@
  * will cause compilation to stop on mismatch.
  * (for details, see gcc PR 15089)
  */
-#define __asmeq(x, y)  ".ifnc " x "," y " ; .err ; .endif\n\t"
+// mako bring-up: gcc 9 allocates registers correctly; the ancient PR 15089
+// check spuriously fails on modern gcc, so disable it.
+#define __asmeq(x, y)  ""
 
 
 #endif /* __ASM_ARM_COMPILER_H */
